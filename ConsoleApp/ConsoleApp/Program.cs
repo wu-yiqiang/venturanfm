17f8d6c55bb3ca009608704519c3ddb07e0589a2
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-10-17 20:45:54 +0800

    init

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var venturan = new Student("张三", 23,1,3,2);
+        Console.WriteLine(venturan.personIsAdult());
+    }
+}
+
+class Person
+{
+    protected string name;
+    protected int age = 0;
+    protected int sex;
+    protected bool isAdult = false;
+    public Person() {
+        Console.WriteLine(this.age);
+        if (this.age >= 18)
+        {
+            this.isAdult = true;
+        }else
+        {
+            this.isAdult = false;
+        }
+    }
+    public bool personIsAdult()
+    {
+        return this.isAdult;
+    }
+}
+
+class Student : Person
+{
+    public int grade;
+    public int classes;
+
+    public Student(string name, int age,int sex, int grade, int classes):base()
+    {
+       this.grade = grade;
+       this.classes = classes;
+       base.name = name;
+       base.age = age;
+       base.sex = sex;
+    }
+}
