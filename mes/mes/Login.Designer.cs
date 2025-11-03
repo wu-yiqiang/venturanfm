21f6e6c653dc6f5c1d3bd384408abfe91c5ac7e1
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-11-03 18:02:37 +0800

    初始化

--- mes/mes/Login.Designer.cs
+++ mes/mes/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace mes
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(500, 600);
+            this.ClientSize = new System.Drawing.Size(400, 480);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MES";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
